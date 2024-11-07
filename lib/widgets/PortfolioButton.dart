2d4f40e685143b8e278bbaeef406f0ec06681438
eneko12rebollo <eneko12rebollo@gmail.com>
2024-11-07 15:52:51 +0000

    ahora si deberia estar

--- lib/widgets/PortfolioButton.dart
+++ lib/widgets/PortfolioButton.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide BoxDecoration, BoxShadow;
+import 'package:flutter_inset_box_shadow/flutter_inset_box_shadow.dart';
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:lottie/lottie.dart';
 
@@ -33,7 +34,7 @@ class _PortfolioButtonState extends State<PortfolioButton> {
           alignment: Alignment.center,
           children: [
             AnimatedContainer(
-              duration: Duration(milliseconds: 300),
+              duration: Duration(milliseconds: 600),
               curve: Curves.easeIn,
               width: 200, // Ancho específico del botón
               height: 100, // Alto específico del botón
@@ -42,7 +43,22 @@ class _PortfolioButtonState extends State<PortfolioButton> {
                     255, 238, 245, 245), // Color del botón y del fondo
                 borderRadius: BorderRadius.circular(12),
                 boxShadow: _isHovered
-                    ? []
+                    ? [
+                        const BoxShadow(
+                            blurStyle: BlurStyle.inner,
+                            color: Color.fromRGBO(50, 50, 93, 0.25),
+                            offset: Offset(0, -30),
+                            blurRadius: 60,
+                            spreadRadius: -12,
+                            inset: true),
+                        const BoxShadow(
+                            blurStyle: BlurStyle.inner,
+                            color: Color.fromRGBO(0, 0, 0, 0.3),
+                            offset: Offset(0, -18),
+                            blurRadius: 36,
+                            spreadRadius: -18,
+                            inset: true),
+                      ]
                     : [
                         BoxShadow(
                           color: Colors.grey.shade500, // Sombra más oscura
@@ -57,58 +73,31 @@ class _PortfolioButtonState extends State<PortfolioButton> {
                       ],
               ),
               padding: EdgeInsets.symmetric(vertical: 20, horizontal: 40),
-              child: SizedBox.expand(
-                child: AnimatedContainer(
-                  duration: const Duration(milliseconds: 300),
-                  curve: Curves.easeIn, // Alto específico del botón
-                  decoration: BoxDecoration(
-                    color: const Color.fromARGB(
-                        255, 238, 245, 245), // Color del botón y del fondo
-                    borderRadius: BorderRadius.circular(12),
-                    boxShadow: _isHovered
-                        ? const [
-                            BoxShadow(
-                              color: Color.fromRGBO(50, 50, 93, 0.25),
-                              offset: Offset(0, -30),
-                              blurRadius: 60,
-                              spreadRadius: -12,
-                            ),
-                            BoxShadow(
-                              color: Color.fromRGBO(0, 0, 0, 0.3),
-                              offset: Offset(0, -18),
-                              blurRadius: 36,
-                              spreadRadius: -18,
-                            ),
-                          ]
-                        : [],
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  AutoSizeText(
+                    widget.title, // Usar el título pasado como parámetro
+                    style: TextStyle(color: Colors.black, fontSize: 24),
+                    maxLines: 1, // Número máximo de líneas
+                    minFontSize: 12, // Tamaño mínimo de la fuente
+                    overflow: TextOverflow
+                        .ellipsis, // Añadir puntos suspensivos si el texto es muy largo
                   ),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      AutoSizeText(
-                        widget.title, // Usar el título pasado como parámetro
-                        style: TextStyle(color: Colors.black, fontSize: 24),
-                        maxLines: 1, // Número máximo de líneas
-                        minFontSize: 12, // Tamaño mínimo de la fuente
-                        overflow: TextOverflow
-                            .ellipsis, // Añadir puntos suspensivos si el texto es muy largo
-                      ),
-                      AnimatedOpacity(
-                        opacity: _isHovered ? 1.0 : 0.0,
-                        duration: Duration(milliseconds: 300),
-                        child: AutoSizeText(
-                          widget
-                              .subtitle, // Usar el subtítulo pasado como parámetro
-                          style: TextStyle(color: Colors.black54, fontSize: 16),
-                          maxLines: 1, // Número máximo de líneas
-                          minFontSize: 12, // Tamaño mínimo de la fuente
-                          overflow: TextOverflow
-                              .ellipsis, // Añadir puntos suspensivos si el texto es muy largo
-                        ),
-                      ),
-                    ],
+                  AnimatedOpacity(
+                    opacity: _isHovered ? 1.0 : 0.0,
+                    duration: Duration(milliseconds: 300),
+                    child: AutoSizeText(
+                      widget
+                          .subtitle, // Usar el subtítulo pasado como parámetro
+                      style: TextStyle(color: Colors.black54, fontSize: 16),
+                      maxLines: 1, // Número máximo de líneas
+                      minFontSize: 12, // Tamaño mínimo de la fuente
+                      overflow: TextOverflow
+                          .ellipsis, // Añadir puntos suspensivos si el texto es muy largo
+                    ),
                   ),
-                ),
+                ],
               ),
             ),
             if (_isHovered)
